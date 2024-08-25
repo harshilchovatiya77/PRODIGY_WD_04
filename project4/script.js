3e8e641231afc8dbbe1d3df77b9f382f09f09d96
harshilchovatiya77 <161808972+harshilchovatiya77@users.noreply.github.com>
2024-08-25 11:00:02 +0530

    Add files via upload
    PROTFOLIO

---
+++ project4/script.js
@@ -0,0 +1,18 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Mobile Navigation Toggle
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
